ed34e2c498de264383f654ba5aa400bc591ccc07
Rahmat Rezeki <rzqbuchek@gmail.com>
2018-07-29 22:19:23 +0700

    Add custom theme drupal

---
+++ themes/immap/js/custom.js
@@ -0,0 +1,59 @@
+jQuery(window).load(function() {
+    
+  /* Navigation */
+
+	jQuery('#main-menu > ul').superfish({ 
+		delay:       500,								// 0.1 second delay on mouseout 
+		animation:   { opacity:'show',height:'show'},	// fade-in and slide-down animation 
+		dropShadows: true								// disable drop shadows 
+	});	  
+
+	jQuery('#main-menu > ul').mobileMenu({
+		prependTo:'.mobilenavi'
+	});
+
+});
+
+
+( function() {
+	$(document).ready(function(){               
+	  $('.materialboxed').materialbox();
+	})
+
+	$(document).ready(function(){
+	  $('.materialboxed').materialbox();
+	})
+	
+	$(document).on('click', '.panel-heading span.clickable', function(e){
+	    var $this = $(this);
+		if(!$this.hasClass('panel-collapsed')) {
+			$this.parents('.panel').find('.panel-body').slideUp();
+			$this.addClass('panel-collapsed');
+			$this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+			
+		} else {
+			$this.parents('.panel').find('.panel-body').slideDown();
+			$this.removeClass('panel-collapsed');
+			$this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+			
+		}
+	})
+
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+	if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
+		var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+		window[ eventMethod ]( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+					element.tabIndex = -1;
+
+				element.focus();
+			}
+		}, false );
+	}
+})();
